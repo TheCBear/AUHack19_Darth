732bcb438d891dec75bf42aa6139a82070ce228a
Andreas Hervert Nielsen <andreas.hervertgmail.com>
2019-04-06 06:11:45 +0200

    hjemmeside oppe

---
+++ socialPicView/socialPicView/FormModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace socialPicView
+{
+    public class SnstagramTag
+    {
+        public string tagSnapChat { get; set; }
+    }
+
+    public class InstagramTag
+    {
+        public string tagInstagram { get; set; }
+    }
+
+    public class TwitterTag
+    {
+        public string tagTwitter { get; set; }
+    }
+}
